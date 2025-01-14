20251e55e20bf2cbe11f0634c5da74a5589643f8
Valerie Ivashkevich <ivashkevich03@gmail.com>
2025-01-14 16:25:45 +0300

    feat: implement header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const Header = () => {
+  const path = usePathname();
+  return (
+    <header className="w-full h-16 bg-blue-500">
+      <nav className="flex items-center h-full pl-16 text-white">
+        <Link
+          className={`flex justify-center items-center h-full px-4 hoverable:hover:underline ${
+            path === "/" ? "bg-blue-600" : ""
+          }`}
+          href="/"
+        >
+          Все котики
+        </Link>
+        <Link
+          className={`flex justify-center items-center h-full px-4 hoverable:hover:underline ${
+            path === "/favs" ? "bg-blue-600" : ""
+          }`}
+          href="favs"
+        >
+          Любимые котики
+        </Link>
+      </nav>
+    </header>
+  );
+};
